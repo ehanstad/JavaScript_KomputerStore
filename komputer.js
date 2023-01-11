1d0eaf7e53a74a13b027a65c40395c05fd2a8a73
Erik <erik_hanstad@hotmail.com>
2023-01-11 17:01:18 +0100

    fix some bugs with fetching

--- komputer.js
+++ komputer.js
@@ -1,6 +1,10 @@
 const computerSelectElement = document.getElementById("laptops");
 const computerFeaturesElement = document.getElementById("laptop-features");
 const computerInformationElement = document.getElementById("information-section");
+const computerImgElement = document.getElementById("laptop-img");
+const computerTitleElement = document.getElementById("laptop-title");
+const computerDescripitionElement = document.getElementById("laptop-discription");
+const computerPriceElement = document.getElementById("laptop-price");
 
 let computers = [];
 
@@ -16,31 +20,35 @@ const addComputersToInventory = (computers) => {
         computerOptionElement.appendChild(document.createTextNode(computer.title));
         computerSelectElement.appendChild(computerOptionElement);
     });
-    selectedComputer = computers[0];
+
+    
 }
 
 const handleComputerInventoryChange = e => {
     const selectedComputer = computers[e.target.selectedIndex];
-    selectedComputer.specs.map(spec => {
+    
+    console.log(computerTitleElement);
+    //console.log(selectedComputer);
+    //console.log(computerDescripitionElement);
+
+    /*selectedComputer.specs.map(spec => {
         const featureElement = document.createElement("p");
         featureElement.innerText = spec;
         computerFeaturesElement.appendChild(featureElement);
-    });
-    console.log(selectedComputer);
-    const computerImgElement = document.createElement("img");
-    const computerTitleElement = document.createElement("h2");
-    const computerDescripitionElement = document.createElement("p");
-    const computerPriceElement = document.createElement("p");
+    });*/
 
-    let img =computerImgElement.src = "https://noroff-komputer-store-api.herokuapp.com/" + selectedComputer.image;
-    console.log(img);
-    computerTitleElement.appendChild(document.createTextNode(selectedComputer.title));
-    computerDescripitionElement.appendChild(document.createTextNode(selectedComputer.description));
-    computerPriceElement.appendChild(document.createTextNode(selectedComputer.price));
+    //const computerImgElement = document.createElement("img");
+    //const computerTitleElement = document.createElement("h2");
+    //const computerDescripitionElement = document.createElement("p");
+    //const computerPriceElement = document.createElement("p");
 
-    computerInformationElement.appendChild(computerImgElement);
-    computerInformationElement.appendChild(computerDescripitionElement);
-    computerInformationElement.appendChild(computerPriceElement);
+    computerImgElement.src = "https://noroff-komputer-store-api.herokuapp.com/" + selectedComputer.image;
+
+    computerTitleElement.innerText = selectedComputer.title;
+    computerDescripitionElement.innerText = selectedComputer.description;
+    computerPriceElement.innerText = selectedComputer.price;
+
+    console.log(computerTitleElement);
 }
 
 computerSelectElement.addEventListener("change", handleComputerInventoryChange);
